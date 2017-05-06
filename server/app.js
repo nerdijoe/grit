dfdea0e288c92b9adb544434b7b553ed18d8dc5a
Rudy Wahjudi <rudy.wahjudi@gmail.com>
2017-05-06 11:11:17 +0700

    user can sign up and sign in

--- server/app.js
+++ server/app.js
@@ -1,8 +1,13 @@
 var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
+const passport = require('passport');
+const Strategy = require('passport-local').Strategy;
+var passwordHash = require('password-hash');
+var cors = require('cors')
 
 var index = require('./routes/index');
+var users = require('./routes/users');
 
 
 var app = express();
@@ -16,6 +21,24 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/', index);
+app.use('/users', users);
+
+passport.use(new Strategy(
+  function(username, password, next) {
+    let User = require('./models/user')
+
+    User.findOne( {username: username}, (err, user) => {
+      if(err) next(err);
+      console.log(user)
+      if(passwordHash.verify(password, user.password)) {
+        next(null, user)
+      } else {
+        next("User entered wrong username and password");
+      }
+    })
+  }
+));
+
 
 app.listen(3000, function () {
   console.log('GRIT is listening on port 3000!')
---
+++ server/routes/users.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+const passport = require('passport');
+var user_controller = require('../controllers/userController')
+
+// list all users
+router.get('/', user_controller.get_users );
+
+router.post('/signup', user_controller.signup);
+router.post('/signin', passport.authenticate('local', {session: false}), user_controller.signin);
+
+
+module.exports = router;
